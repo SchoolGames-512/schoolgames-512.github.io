0d9b40fd477357cdc3c12c562e26b9538d071e69
discoveryX64 <119447512+discoveryX64@users.noreply.github.com>
2024-02-07 15:17:10 +0000

    7

--- assets/js.js
+++ assets/js.js
@@ -1,5 +1,21 @@
-window.addEventListener("keydown", function(e) {
-    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
-        e.preventDefault();
+// No scrolling with arrow keys
+
+window.addEventListener('keydown', (e) => {
+    if (e.target.localName != 'input') {   // if you need to filter <input> elements
+        switch (e.keyCode) {
+            case 37: // left
+            case 39: // right
+                e.preventDefault();
+                break;
+            case 38: // up
+            case 40: // down
+                e.preventDefault();
+                break;
+            default:
+                break;
+        }
     }
-}, false);
+}, {
+    capture: true,   // this disables arrow key scrolling in modern Chrome
+    passive: false   // this is optional, my code works without it
+});
